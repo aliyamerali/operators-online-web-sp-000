58d2dee6b58dca462ba4e1b969fe69f1a1485a7d
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-19 02:51:41 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-	if speed.between(40, 60)
+	if speed.between?(40, 60)
 		return true
 	else
 		return false
@@ -9,5 +9,5 @@ end
 
 
 def not_safe?(speed)
-	speed.between(40,60) ? true : false
+	speed.between?(40,60) ? true : false
 end
